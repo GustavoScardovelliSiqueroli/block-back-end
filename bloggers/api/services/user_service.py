968e94cc2ff16ced47da887c591ea857f3b540fc
Gustavo Scardovelli Siqueroli <55260462+GustavoScardovelliSiqueroli@users.noreply.github.com>
2023-12-01 01:51:53 -0300

    add padrao para seguir

---
+++ bloggers/api/services/user_service.py
@@ -0,0 +1,13 @@
+class UserService():
+    pass
+    def teste(self, txt: str) -> bool:
+        """Verifica se existe um texto de parâmetro
+        retunr: True
+        return: False"""
+        if txt:
+            return bool(True)
+        return bool(False)
+    
+    teste()
+        
+            
